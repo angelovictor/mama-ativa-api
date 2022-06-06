b26bd7699a4f51e66c78fc6a1d48d40aa0c5152a
Ângelo Victor <angelovictor.ti@gmail.com>
2022-06-06 14:03:32 -0300

    Add script for sync database and CORS

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 "use strict";
+require("dotenv").config();
 var express = require("express");
 const app = express();
+const cors = require("cors");
+const db = require("./app/models");
+
+var corsOptions = {
+  origin: `${process.env.APP_CORS}`
+};
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
@@ -24,9 +33,38 @@ app.get("/*", (req, res) => {
   res.status(404).send(" ");
 });
 
+// database
+/*const Role = db.role;
+
+//db.sequelize.sync();
+// force: true will drop the table if it already exists
+db.sequelize.sync({ force: true }).then(() => {
+  console.log('Drop and Resync Database with { force: true }');
+  initial();
+});
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+
+  Role.create({
+    id: 2,
+    name: "mod"
+  });
+
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
+initial();*/
+
 const port = process.env.PORT || 8080
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}\n`)
 })
 
 module.exports = app;
