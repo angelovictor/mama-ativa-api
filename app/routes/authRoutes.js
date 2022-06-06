f0f931c1b47501449834a3c90b19bf137c9dfd6b
Ângelo Victor <angelovictor.ti@gmail.com>
2022-06-06 14:12:16 -0300

    Add routes for put and delete between users and addresses

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,9 +1,9 @@
 "use strict";
-const { verifySignUp } = require("../middleware");
+const { verifySignUp, authJwt } = require("../middleware");
 const controller = require("../controllers/authController");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
@@ -20,5 +20,19 @@ module.exports = function(app) {
     controller.signup
   );
 
-  app.post("/api/auth/signin", controller.signin);
+  app.post(
+    "/api/auth/signin", controller.signin
+  );
+
+  app.put(
+    "/api/auth/profile/:id",
+    [authJwt.verifyToken],
+    controller.profileUpdate
+  );
+
+  app.delete(
+    "/api/auth/profile/:id",
+    [authJwt.verifyToken],
+    controller.profileDelete
+  );
 };
--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -2,8 +2,8 @@
 const { authJwt } = require("../middleware");
 const controller = require("../controllers/userController");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
