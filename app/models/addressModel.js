c778903c7200551b48712e4661f95325791e7ed6
Ângelo Victor <angelovictor.ti@gmail.com>
2022-06-06 14:15:17 -0300

    Updates in atributes definitions between models Users and Addresses

--- app/models/addressModel.js
+++ app/models/addressModel.js
@@ -3,21 +3,25 @@ module.exports = (sequelize, Sequelize) => {
     const Address = sequelize.define("addresses", {
         cep: {
             type: Sequelize.INTEGER(9),
+            allowNull: false
         },
         street: {
             type: Sequelize.STRING(60),
+            allowNull: false
         },
         district: {
             type: Sequelize.STRING(40),
+            allowNull: false
         },
         city: {
             type: Sequelize.STRING(40),
+            allowNull: false
         },
         state: {
             type: Sequelize.STRING(2),
+            allowNull: false
         }
     });
-  
+
     return Address;
 };
-  
